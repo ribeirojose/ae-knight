2034985845b17e6471c8d6ab5a6ca3569028f7dd
José Ribeiro <ribeirojose@me.com>
2019-12-24 13:39:13 -0300

    Disable auth middleware

--- api/src/middlewares/ensure-authenticated.ts
+++ api/src/middlewares/ensure-authenticated.ts
@@ -1,17 +1,18 @@
-import { db } from '../services/knex-connection'
+// import { db } from '../services/knex-connection'
 
 export const ensureAuthenticated = async (req, res, next) => {
   const token = req.headers.authorization || (req.session.passport && req.session.passport.user.token)
 
-  if (token) {
-    const validToken = await db('auth_tokens')
-      .where({ token })
-      .first()
+  return next()
+  // if (token) {
+  //   const validToken = await db('auth_tokens')
+  //     .where({ token })
+  //     .first()
 
-    if (validToken && validToken.status === 'active') {
-      return next()
-    }
-  }
+  //   if (validToken && validToken.status === 'active') {
+  //     return next()
+  //   }
+  // }
 
   res.sendStatus(401)
 }
