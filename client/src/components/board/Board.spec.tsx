cbd299fac6a8da81c6e98b7438be4db296c7fcda
José Ribeiro <ribeirojose@me.com>
2019-12-27 16:54:58 -0300

    Add store to fix Board and Chess tests

--- client/src/components/board/Board.spec.tsx
+++ client/src/components/board/Board.spec.tsx
@@ -1,14 +1,16 @@
 import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import * as React from 'react'
+import { initialState } from '../../redux/ducks/chess'
 
 import Board from './Board'
 
 configure({ adapter: new Adapter() })
 
 describe(`<Board />`, () => {
+
   it('renders', () => {
-    const boardComponent = shallow(<Board />)
+    const boardComponent = shallow(<Board {...initialState} />)
 
     expect(boardComponent.exists()).toBeTruthy()
   })
